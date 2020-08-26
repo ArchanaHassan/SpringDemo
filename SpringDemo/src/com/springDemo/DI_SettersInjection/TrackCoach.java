9aa61e3b27cf0fe28be79fd35e78ceea6ec27ff9
Archana <archanakl.05@gmail.com>
2020-08-26 11:26:28 +0530

    added sout

--- SpringDemo/src/com/springDemo/DI_SettersInjection/TrackCoach.java
+++ SpringDemo/src/com/springDemo/DI_SettersInjection/TrackCoach.java
@@ -4,10 +4,11 @@ public class TrackCoach implements Coach {
    private FortuneService fortuneService;
    
 	public TrackCoach() {
-	
+							System.out.println("No arguments constructor");
                         }
 	
 	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println("Inside setter methods - setFortuneService");
 		fortuneService = theFortuneService;
 	}
 
